8aa66ae458e6a79c3e4c5ea2a8ec08c3093c3ec5
bluzer2001 <bluzer@yandex.ru>
2022-04-12 22:51:20 +0300

    Добавляю рисовалку в колаб

--- solver_core/regression/drawing/draw_func.py
+++ solver_core/regression/drawing/draw_func.py
@@ -6,7 +6,7 @@ from plotly.subplots import make_subplots
 from typing import Callable, Optional
 
 from ..linear_regression import LinearRegression
-from ..regression.exponential_regression import ExpRegression
+from ..exponential_regression import ExpRegression
 from ..polynomial_regression import polynomial_regression
 
 
