d0a86b4fda980ed5125e957d51555ff95b18b288
nadya <nadya.geraskina@gmail.com>
2022-05-09 19:17:37 +0300

    Исправлены недочеты, проведено стартовое тестирование

--- solver_core/classification/handlers/logistic_regression.py
+++ solver_core/classification/handlers/logistic_regression.py
@@ -17,6 +17,15 @@ class LogisticRegression:
     X_test: np.ndarray
         Массив данных, на которых тестируется модель. Может быть одномерными и многомерными.
 
+    mu: Optional[float] = 0.5
+        Критерий, по которому отделяются классы в логистической регрессии.
+
+    max_iter: Optional[int] = 500
+        Максимальное количество итераций алгоритма.
+
+    delta_w: Optional[float] = 100
+        Параметр остановки алгоритма. Как только разница между результатами соседним итераций меньше чем данный параметр, алгоритм прекращает работу.
+
     alpha: Optional[float] = 0.5
         Степень влияния регуляризации. На этот коэфициент домножается регуляризация.
 
@@ -32,9 +41,10 @@ class LogisticRegression:
                  X_train: np.ndarray,
                  y_train: np.ndarray,
                  X_test: np.ndarray,
+                 mu: Optional[float] = 0.5,
                  alpha: Optional[float] = 0.5,
-                 delta_w: Optional[float] = 0.001,
-                 lam: Optional[float] = 0.001,
+                 delta_w: Optional[float] = 100,
+                 max_iter: Optional[int] = 500,
                  regularization: Optional[bool] = False,
                  draw_flag: Optional[bool] = False):
         self.X_train = X_train
@@ -44,7 +54,9 @@ class LogisticRegression:
         self.draw_flag = draw_flag
         self.omega = np.zeros(X_train.shape[1] + 1)
         self.delta_w = delta_w
+        self.max_iter = max_iter
         self.alpha = alpha
+        self.mu = mu
 
     def solve(self):
         """
@@ -59,25 +71,58 @@ class LogisticRegression:
             Коэфициенты регрессии w.
 
         """
+        self.X_train = np.concatenate((np.ones_like(X_train), X_train), axis=1)
         self.omega = np.linalg.inv(self.X_train.T @ self.X_train) @ self.X_train.T @ self.y_train
-        for i in range(len(self.omega)):
-            if (self.omega[i + 1] - self.omega[i]) ** 2 > self.delta_w:
-                z = self.X_train @ self.omega
-                p = 1 / (1 + np.exp(-z))
-                g = p * (1 - p)
-                u = z + (self.y_train - p) / g
-                G = np.zeros((len(g), len(g)), float)
-                np.fill_diagonal(G, g)
-                E = np.zeroes((len(g), len(g)), int)
-                np.fill_diagonal(E, 1)
-                if self.regularization:
-                    self.omega = np.linalg.inv(self.X_train.T @ G @ self.X_train + self.alpha@E) @ self.X_train @ G @ u
-                else:
-                    self.omega = np.linalg.inv(self.X_train.T @ G @ self.X_train) @ self.X_train @ G @ u
+        # print(self.omega)
+        i = 0
+        old_w = np.array([-(10 ** 3)] * len(self.omega))
+        while np.sum((old_w - self.omega) ** 2) > self.delta_w and i < self.max_iter:
+            # print('norma',np.sum((old_w - self.omega) ** 2))
+            z = self.X_train @ self.omega
+            # print('z=',z)
+            p = 1 / (1 + np.exp(-z))
+            # print('p=',p)
+            g = p * (1 - p)
+            # print('g=',g)
+            u = z + (self.y_train - p) / g
+            # print('u=',u)
+            G = np.zeros((len(g), len(g)), float)
+            np.fill_diagonal(G, g)
+            # print('G=',G)
+            E = np.zeros((g.shape[1], g.shape[1]), int)
+            np.fill_diagonal(E, 1)
+            old_w = self.omega
+            # print('X_train=',self.X_train.shape)
+            if self.regularization:
+                self.omega = np.linalg.inv(
+                    self.X_train.T @ G @ self.X_train + self.alpha * E) @ self.X_train.T @ G @ u
+            else:
+                self.omega = np.linalg.inv(self.X_train.T @ G @ self.X_train) @ self.X_train.T @ G @ u
+            i += 1
 
-        z = self.omega[0] + self.X_test@self.omega[1:]
+        # print('omega = ',self.omega)
+        # print('X_test',self.X_test )
+        z = self.omega[0] + self.X_test @ self.omega[1:]
+        # print('z_last',z)
         y_pred = 1 / (1 + np.exp(-z))
-        return np.concatenate((self.X_test,y_pred),axis=1), self.omega
+        # print('y_pred',y_pred)
+        mu = np.mean(y_pred.flatten())
+        print('mu = ', mu)
+        # print(y_pred)
+        y_pred = np.array([1 if i[0] >= mu else 0 for i in y_pred]).reshape((-1, 1))
+        # print(y_pred)
+        # print('omega', self.omega)
+        # print('res', np.concatenate((self.X_test, y_pred), axis=1))
+        return np.concatenate((self.X_test, y_pred), axis=1), self.omega
+
 
 if __name__ == "__main__":
-
+    X = np.random.randint(21, size=(100, 1))
+    y = np.array([1 if i > 10 else 0 for i in X]).reshape((-1, 1))
+    X_train = X[:80, :]
+    y_train = y[:80, :]
+    X_test = X[80:, :]
+    y_test = y[80:, :]
+    pred, omega = LogisticRegression(X_train, y_train, X_test, regularization=True).solve()
+    print(pred, y_test)
+    print(omega)
