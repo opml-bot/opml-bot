3a2dee4d4764ffb782c17b92d99c7fe3ba2dbe60
bluzer <bluzer@yandex.ru>
2022-06-07 23:18:32 +0300

    Доработка метода первой фазы

--- solver_core/inner_point/handlers/prepocessing.py
+++ solver_core/inner_point/handlers/prepocessing.py
@@ -113,7 +113,7 @@ def prepare_all(function: str, restriction: str, method: str, started_point: Opt
         for i in range(5):
             try:
                 point = FirstPhase(n_vars, rewrited_restrs).solve()
-            except (np.linalg.LinAlgError, OverflowError):
+            except (np.linalg.LinAlgError, OverflowError) as e:
                 print(f'{i+1} попытка найти начальную точку провалилась, попробуем запустить новую итерацию.')
             else:
                 break
