1ff669c82145812a43c3e2294eef927310d3e899
Dmitry <dru4inin.dmitry@yandex.ru>
2022-06-19 01:55:54 +0300

    Обновления для третьей тз

--- vk_bot/answerer/grad/grad.py
+++ vk_bot/answerer/grad/grad.py
@@ -2,7 +2,7 @@ from vk_bot.database import BotDatabase
 from vk_bot.sql_queries import Select, Insert, Update
 
 
-class OneDimOpt:
+class Grad:
     def __init__(self, db: BotDatabase, user_id: int):
         self.db = db
         self.user_id = user_id
