d91ba0ccf58b0c60112d04a4da67214dd3238227
bluzer <bluzer@yandex.ru>
2022-06-19 01:14:06 +0300

    Чет туплю)

--- solver_core/integer_LP/handlers/prepocessing.py
+++ solver_core/integer_LP/handlers/prepocessing.py
@@ -86,7 +86,7 @@ def prepare_all(function: str,
             strfunc = strfunc.replace(str_x, str_x[:1] + '_' + str_x[1:])
         strfunc = strfunc.replace('*', '')
         restriction = [i.replace('*', '').replace(' x', ' 1x') for i in restriction]
-        return len(vars_), restriction, (strfunc, opt_type)
+        return len(vars_), restriction, (opt_type, strfunc)
 
 def to_callable(expression: sympy.core) -> Callable:
     """
